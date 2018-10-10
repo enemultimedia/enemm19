21a8e240437429e90d2348d6e61a19e4048bad4f
Pedro Janeiro <pjaneiro@whitesmith.co>
2018-10-10 19:36:20 +0100

    Fix forms and make mailchimp form async

---
+++ assets/js/form_submit.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  var $form = $('#mailchimp_form');
+  $form.submit(function(event){
+    event.preventDefault();
+    register($form);
+  });
+})
+
+function register($form) {
+  $('input[type=submit]', $form).val('Sending...');
+  $.ajax({
+    type: $form.attr('method'),
+    url: $form.attr('action'),
+    data: $form.serialize(),
+    cache: false,
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8',
+    error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
+    success: function (data) {
+      $('input[type=submit]', $form).val('Subscrever')
+      if (data.result === 'success') {
+        // Yeahhhh Success
+        $form.replaceWith(
+          $('<p>E-mail registado com sucesso</p>')
+        );
+      } else {
+        // Something went wrong, do something to notify the user.
+        $form.replaceWith(
+          $('<p>Erro a registar e-mail</p>')
+        );
+      }
+    }
+  })
+};
