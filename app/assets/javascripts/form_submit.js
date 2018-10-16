57eab73cc2ae2ed5b1b7cc47f5ed3dd54e8a539b
Pedro Janeiro <pjaneiro@whitesmith.co>
2018-10-16 23:09:11 +0100

    Change forms behaviour Don't delete form, simply erase its content on success. Should add some message on either success/failure

--- app/assets/javascripts/form_submit.js
+++ app/assets/javascripts/form_submit.js
@@ -4,6 +4,10 @@ $(document).ready(function () {
     event.preventDefault();
     register($form);
   });
+
+  $('#new_talk_suggestion').submit(function(event){
+    $('input[type=submit]', '#new_talk_suggestion').val('A gravar...');
+  });
 })
 
 function register($form) {
@@ -17,17 +21,13 @@ function register($form) {
     contentType: 'application/json; charset=utf-8',
     error: function (err) { alert('Erro a ligar ao servidor.') },
     success: function (data) {
-      $('input[type=submit]', $form).val('Subscrever')
+      $('input[type=submit]', $form).val('Subscrever');
       if (data.result === 'success') {
         // Yeahhhh Success
-        $form.replaceWith(
-          $('<p>E-mail registado com sucesso</p>')
-        );
+        $form[0].reset();
       } else {
         // Something went wrong, do something to notify the user.
-        $form.replaceWith(
-          $('<p>Erro a registar e-mail</p>')
-        );
+        $form[0].reset();
       }
     }
   })
--- app/models/talk_suggestion.rb
+++ app/models/talk_suggestion.rb
@@ -1,4 +1,2 @@
 class TalkSuggestion < ApplicationRecord
-  validates :subject_or_speaker, presence: true
-  validates :tell_us_more, presence: true
 end
