aeebe1a235319f22a3374766587ec98e528885eb
Pedro Janeiro <pjaneiro@whitesmith.co>
2018-12-05 18:29:58 +0000

    Update copy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :talk_suggestions, only: :create
-  get 'pages/index'
-  get 'pages/about'
+  get 'index', to: 'pages#index', as: 'index'
+  get 'about', to: 'pages#about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
