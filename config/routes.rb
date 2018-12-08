997cdce151489510c302c8b4517b655e1ef0039a
Pedro Janeiro <pjaneiro@whitesmith.co>
2018-12-08 18:26:50 +0000

    Static speakers page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :talk_suggestions, only: :create
   get 'index', to: 'pages#index', as: 'index'
   get 'about', to: 'pages#about', as: 'about'
+  get 'speakers', to: 'pages#speakers', as: 'speakers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
