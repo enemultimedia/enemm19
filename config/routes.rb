6c669bf0dd84f4a595156e29de323c60d419b660
Pedro Janeiro <pjaneiro@whitesmith.co>
2018-12-08 18:47:23 +0000

    Open specific speaker link

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'index', to: 'pages#index', as: 'index'
   get 'about', to: 'pages#about', as: 'about'
   get 'speakers', to: 'pages#speakers', as: 'speakers'
+  get 'speakers/:slug', to: 'pages#speakers', as: 'speaker'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
