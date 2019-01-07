b5fb3a88fce720fec2e967c754c4edcc62f1a8d2
Pedro Janeiro <pjaneiro@whitesmith.co>
2019-01-07 04:01:08 +0000

    Activate activestorage to allow image uploads

--- app/admin/speakers.rb
+++ app/admin/speakers.rb
@@ -1,11 +1,13 @@
 ActiveAdmin.register Speaker do
-  permit_params :name, :slug, :image_url, person_infos_attributes: [:person_id, :category, :content, :order, :id, :_destroy]
+  permit_params :name, :slug, :image, person_infos_attributes: [:person_id, :category, :content, :order, :id, :_destroy]
 
   show do
     attributes_table do
       row :name
       row :slug
-      row :image_url
+      row :image do |s|
+        image_tag(url_for(s.image), height: '256') unless s.image.nil?
+      end
     end
     panel 'Info' do
       table_for speaker.person_infos do
@@ -21,7 +23,9 @@ ActiveAdmin.register Speaker do
     f.inputs 'Details' do
       f.input :name
       f.input :slug
-      f.input :image_url
+      f.input :image, as: :file, :hint => f.object.image.present? \
+                                  ? image_tag(url_for(f.object.image), height: '256') \
+                                  : content_tag(:span, "No image yet")
     end
 
     f.inputs do
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,5 @@
 class Person < ApplicationRecord
   has_many :person_infos, -> { order(order: :asc) }
+  has_one_attached :image
   accepts_nested_attributes_for :person_infos, allow_destroy: true
 end
