fa8e167fd03148a8b09c9a83f54fb17e6ab784b1
Pedro Janeiro <pjaneiro@whitesmith.co>
2019-01-12 18:25:11 +0000

    Make basis

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'speakers', to: 'pages#speakers', as: 'speakers'
   get 'speakers/:slug', to: 'pages#speakers', as: 'speaker'
   get 'ambassadors', to: 'pages#ambassadors', as: 'ambassadors'
+  get 'schedule', to: 'pages#schedule', as: 'schedule'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
 end
